ef43ad41e929878c6972a2ead2bcf89a8920b468
adityav2104 <aditya.verma@gammaedge.io>
2025-08-18 15:26:59 +0530

    Modified my code with the use of .env

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
-GOOGLE_API_KEY = "AIzaSyAU45zRW0NGCz9_p_7BbICvPNxT8sc-ukM"
+import os
+from dotenv import load_dotenv
+
 
 db_path = "faiss_db"
 csv_path = "data/cars.csv"
 json_path = "qa_Automotive.json"
 csv2_path = "data/cars2.csv"
+
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
